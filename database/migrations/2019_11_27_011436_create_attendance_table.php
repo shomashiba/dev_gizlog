58b3b0f52bc038d389a491cd38b3b7d1e5af7eaf
issei6614 <issei6614@gmail.com>
2019-11-27 11:40:32 +0900

    attendanceテーブルを作成しました。

---
+++ database/migrations/2019_11_27_011436_create_attendance_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('is_absent')->default(0);
+            $table->integer('is_request')->default(0);
+            $table->string('absent_reason', 500)->nullable();
+            $table->string('request_reason', 500)->nullable();
+            $table->date('date')->nullable();
+            $table->datetime('start_time')->nullable();
+            $table->datetime('end_time')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance');
+    }
+}
